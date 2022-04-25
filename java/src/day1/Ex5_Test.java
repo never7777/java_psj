b3a26525356826225f35fabaf24fa237706e3574
never7777 <79982654+never7777@users.noreply.github.com>
2022-04-25 17:17:57 +0900

    자바 1일차 : Test

---
+++ java/src/day1/Ex5_Test.java
@@ -0,0 +1,33 @@
+package day1;
+
+public class Ex5_Test {
+	public static void main(String[] args) {
+		/* 과일가게에서 과일 판매량을 기록하려한다.
+		 * 사과, 오렌지 판매량을 기록하기 위한 변수를 선언하세요
+		 */
+		
+		int apple = 0;
+		int orange = 0;
+		
+		/*2-1 학생 한명의 국어, 영어, 수학 성적을 저장하려 한다. 
+		 * 	  이 때 필요한 변수를 선언하세요.
+		 * 	  성적은 정수로 관리된다.(추가)
+		 */
+		
+		int KorScore = 0;
+		int mathScore = 0 ;
+		int engScore = 0;
+		
+		/*2-2. 학생의 학점을 출력하려고 하낟. 
+		 * 	   이 때 학점을 저장하는 변수를 선언하세요.
+		 *	   학점은 0~4.5로 관리된다.
+		 *	   학점은 F~A로 관리된다.
+		 */
+		
+		float Score1 = 0.0f;
+		char Score2='F';
+		}
+		
+	}
+
+
