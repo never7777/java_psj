f6ed907eac74288b621b7bfdfb53e40b1a5aa89f
never7777 <79982654+never7777@users.noreply.github.com>
2022-04-28 23:45:14 +0900

    자바 4일차 : 무한루프 예제

---
+++ java/src/day4/Ex8_InfiniteLoop.java
@@ -0,0 +1,11 @@
+package day4;
+
+public class Ex8_InfiniteLoop {
+	public static void main(String[] args) {
+		//1부터 int 최대 정수까지 반복함 =>많이 실행할 뿐 무한루프는 아님. 무한루트인 경우 반복문 아래 코드가 에러가 됨
+		for(int i =1; i>0;i++) {
+			//System.out.println(i);
+		}
+		System.out.println("안녕하세요");
+	}
+}
