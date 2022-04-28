44595cc24bf5c16a7121ebf3f294b254e4b1108a
never7777 <79982654+never7777@users.noreply.github.com>
2022-04-28 23:44:40 +0900

    자바 4일차 : break문 예제

---
+++ java/src/day4/Ex4_Break1.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class Ex4_Break1 {
+	public static void main(String[] args) {
+		
+		for(int i = 1;i<=5;i++) {
+			System.out.println("Hello");
+			if(i==3) {
+				break;	
+			}
+		}
+	}
+}
