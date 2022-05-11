945c1a2bafcda7131349e3d4bcad63b1d3d5e9a0
never7777 <79982654+never7777@users.noreply.github.com>
2022-05-11 23:40:40 +0900

    자바 11일차 : final 변수 예제

---
+++ java/src/day11/Ex4_Final.java
@@ -0,0 +1,25 @@
+package day11;
+
+public class Ex4_Final {
+
+	public static void main(String[] args) {
+	System.out.println(Math.PI);
+	System.out.println(Integer.MAX_VALUE);
+	}
+
+}
+class Tv{
+	private final static int MIN_CHANNEL =1;
+	private int Channel;
+	public void print() {
+		System.out.println("최소 채널 : " + MIN_CHANNEL);
+		System.out.println("현재 채널 : " + Channel);
+		
+	}
+	public Tv(int channel) {
+		this.Channel = channel;
+		//minChannel = 2;//에러발생 .final이 붙은 변수를 수정하려고 했기 때문에
+		
+	}
+	
+}
