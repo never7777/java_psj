5ac3f54b40140b9ecb07d73a10d8272922fbd29e
never7777 <79982654+never7777@users.noreply.github.com>
2022-05-13 23:56:12 +0900

    자바 14일차 : 숫자 야구 게임 예제(과제)

---
+++ java/src/day14/Ex2_BaseballGameMain.java
@@ -0,0 +1,13 @@
+package day14;
+
+import java.util.Scanner;
+
+public class Ex2_BaseballGameMain {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+		BaseballGame baseballgame= new BaseballGame(scan);
+		
+		
+	}
+}
