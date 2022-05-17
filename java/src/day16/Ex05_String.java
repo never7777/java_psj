599d15b90f1c153eddb914b4cc3b8bb3d451fec5
never7777 <79982654+never7777@users.noreply.github.com>
2022-05-17 23:43:53 +0900

    자바 16일차 : String 예제 - 검색 예제

---
+++ java/src/day16/Ex05_String.java
@@ -0,0 +1,31 @@
+package day16;
+
+public class Ex05_String {
+	public static void main(String[] args) {
+		String str = "Hello wolrd wolrd";
+		String search = "wo";
+		/* - 문자열.indexOf(검색어)
+		/*   - 일치하는 문자열이 없으면 -1, 있으면 시작번지
+		 *   - 여러개 있어서도 처음 시작하는 번지를 찾음 
+		 *    문자열 (검색어, 시작번지)
+		 *   -문자열에서 시작번지부터 검색어를 찾아 있으면 시작번지, 없으면 -1 
+		 */
+		int index = str.indexOf(search);
+		System.out.println(index);
+		if(index >=0) {
+			System.out.println(str+" 문자열에 "+search+" 문자열은 "+index+ "번지에 있습니다.");
+			index =str.indexOf(search, index+search.length());
+			if(index>=0) {
+				System.out.println(str+" 문자열에 "+search+"문자열은" +index +"번지에 2개 있습니다.");
+			}else {
+				System.out.println(str+" 문자열에 "+search+"문자열은" +index +"번지에 1개 있습니다.");
+			}
+		}else {
+			System.out.println(str+" 문자열에 "+search+" 문자열은 "+"없습니다.");
+		}
+		//뒤에 부터
+		System.out.println(str.lastIndexOf(search));
+		//있다 없다만 알려줌
+		System.out.println(str.contains(search));
+	}
+}
