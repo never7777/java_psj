6050a80bfe203e7a8bc4be0a0a363a516640d408
never7777 <79982654+never7777@users.noreply.github.com>
2022-08-03 23:50:38 +0900

    spring : DB 연동 테스트

--- spring_psj/src/main/java/kr/green/spring/service/MemberService.java
+++ spring_psj/src/main/java/kr/green/spring/service/MemberService.java
@@ -1,7 +1,15 @@
 package kr.green.spring.service;
 
+import kr.green.spring.vo.MemberVO;
+
 public interface MemberService {
 
-	String getEmail(String id);
+	String getEmail(String me_id);
+
+	MemberVO getMember(String me_id);
+
+	MemberVO getMember(MemberVO member);
+
+	MemberVO getMember2(MemberVO member);
 
 }
