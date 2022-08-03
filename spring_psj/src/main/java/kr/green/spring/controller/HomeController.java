030a7064bd7373ff2f63e5efd20b69bb5eb5342f
never7777 <79982654+never7777@users.noreply.github.com>
2022-08-03 15:09:45 +0900

    spring : lombok 추가 및 DB 연동

---
+++ spring_psj/src/main/java/kr/green/spring/controller/HomeController.java
@@ -0,0 +1,85 @@
+package kr.green.spring.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import kr.green.spring.service.MemberService;
+import kr.green.spring.vo.MemberVO;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@Autowired
+	MemberService memberService;
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	/* 접근제한자 public -고정
+	 * 리턴타입 : ajax를 이용한 경우를 제외하고는기본적으로 ModelAndView
+	 * 메소드명 : url 경로와 처리 방식(GET/POST)에서 따오는게 편함
+	 * 				 예로, url /login이고 get방식이면 loginGet으로 짓는게 편함
+	 * 매개변수 : ajax를 제외하고는 기본적으로 ModelAndViwe mv는 들어가고, 다른 매개변수는 추가로 올 수 있음
+	 * 예외처리 : 안해도 됨
+	 * */
+	/* mv.setViewName(문자열)
+	 * 	- ViewResolver로 보낼 문자열을 설정 
+	 *  - redirect, forward가 있는 경우는 url로 이동 
+	 *  	예 : "redirect:/" 또는 "forward:/"
+	 * mv.addObject("화면에서 사용할 이름", 변수/객체) 
+	 *	- 화면에서 사용할 데이터를 이름과 함께 추가
+	 * */
+	/*@ RequestMapping
+	 * value : 처리할 url, {"url1", "url2"}을 이용하여 여러 url을 하나로 처리할 수 있다
+	 * method: url 요청 방식,  RequestMethod.GET/RequestMethod.POST
+	 * 				 생략하면 둘다 처리 
+	 * 
+	 * */
+	/*화면에서 hobby와 time을 안보내면, null이 자동으로 들어감
+	 * 이 때, hobby와 time의 타입이 null을 처리할 수 없는 타입이면 에러가 발생
+	 * */
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView home(ModelAndView mv, String hobby, Integer time){
+    mv.setViewName("/main/home");
+    mv.addObject("name", "홍길동");
+    mv.addObject("age", 20);
+    System.out.println("취미는 " + hobby + "이고, " + time +"시간씩 합니다.");
+      return mv;
+	}
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView homePost(ModelAndView mv, String hobby, Integer time){
+    mv.setViewName("/main/home");
+    System.out.println("취미는 " + hobby + "이고, " + time +"시간씩 합니다.");
+    return mv;
+	}
+	@RequestMapping(value="/hobby/{hobby1}/{time1}")
+	public ModelAndView hobby(ModelAndView mv,
+			@PathVariable("hobby1") String hobby, 
+			@PathVariable("time1") Integer time){
+    mv.setViewName("redirect:/");
+    System.out.println("취미는 " + hobby + "이고, " + time +"시간씩 합니다.");
+    MemberVO member = new MemberVO();
+    
+    return mv;
+	}
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public ModelAndView loginGet(ModelAndView mv){
+    mv.setViewName("/main/login");
+    return mv;
+	}
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public ModelAndView loginPOST(ModelAndView mv, MemberVO member){
+    mv.setViewName("redirect:/login");
+    System.out.println(member); 
+    String email = memberService.getEmail(member.getMe_id());
+    System.out.println("이메일 : "+email);
+    return mv;
+	}
+}
---
+++ spring_psj/src/main/webapp/WEB-INF/views/main/login.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<div class="container">
+		<form action="/spring/login" class="mt-3" method="post">
+		<h1>로그인</h1>
+		<div class="form-group">
+    	<input type="text" class="form-control" name ="me_id" placeholder="아이디">
+		</div>
+		<div class="form-group">
+		  <input type="password" class="form-control" name="me_pw" placeholder="비밀번호">
+		</div>
+		<button class="btn btn-outline-success col-12 mb-3">전송</button>
+	</form>
+	</div>
+</body>
+</html>
