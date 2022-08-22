8dbac707d979aebee136c5063947df19a18e7f36
never7777 <79982654+never7777@users.noreply.github.com>
2022-08-22 15:45:57 +0900

    spring 복습 : 인터셉터를 이용한 페이지 접근 권한 설정 완료

--- spring_psj_test/src/main/java/kr/green/springtest/interceptor/MemberInterceptor.java
+++ spring_psj_test/src/main/java/kr/green/springtest/interceptor/MemberInterceptor.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class MemberInterceptor extends HandlerInterceptorAdapter{
+public class MemberInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, 
 			HttpServletResponse response, 
